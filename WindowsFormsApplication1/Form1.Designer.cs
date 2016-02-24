09bdbe89a1afa0ce276b4d36b619c8612b822336
Martino Jones <martinojones_2009@hotmail.com>
2016-02-23 21:35:51 -0500

    Changed a lot
    I changed the name of the program to Windows Application Manager. Added
    an application count. Changed the UI around. Added uninstall option.
    Changed font size of the application names.

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,66 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ipLabel = new System.Windows.Forms.Label();
-            this.ipAddressLabel = new System.Windows.Forms.Label();
-            this.hostName = new System.Windows.Forms.Label();
-            this.hostNameLabel = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.installedApplicationsLabel = new System.Windows.Forms.Label();
             this.searchBox = new System.Windows.Forms.TextBox();
             this.searchLabel = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.uninstallButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // ipLabel
-            // 
-            this.ipLabel.AutoSize = true;
-            this.ipLabel.Location = new System.Drawing.Point(133, 31);
-            this.ipLabel.Name = "ipLabel";
-            this.ipLabel.Size = new System.Drawing.Size(72, 13);
-            this.ipLabel.TabIndex = 0;
-            this.ipLabel.Text = "IP ADDRESS";
-            // 
-            // ipAddressLabel
-            // 
-            this.ipAddressLabel.AutoSize = true;
-            this.ipAddressLabel.Location = new System.Drawing.Point(9, 31);
-            this.ipAddressLabel.Name = "ipAddressLabel";
-            this.ipAddressLabel.Size = new System.Drawing.Size(118, 13);
-            this.ipAddressLabel.TabIndex = 1;
-            this.ipAddressLabel.Text = "Your IP Address (Main):";
-            // 
-            // hostName
-            // 
-            this.hostName.AutoSize = true;
-            this.hostName.Location = new System.Drawing.Point(104, 54);
-            this.hostName.Name = "hostName";
-            this.hostName.Size = new System.Drawing.Size(71, 13);
-            this.hostName.TabIndex = 2;
-            this.hostName.Text = "HOST NAME";
-            // 
-            // hostNameLabel
-            // 
-            this.hostNameLabel.AutoSize = true;
-            this.hostNameLabel.Location = new System.Drawing.Point(9, 54);
-            this.hostNameLabel.Name = "hostNameLabel";
-            this.hostNameLabel.Size = new System.Drawing.Size(88, 13);
-            this.hostNameLabel.TabIndex = 3;
-            this.hostNameLabel.Text = "Your Host Name:";
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.HorizontalScrollbar = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 231);
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 88);
             this.listBox1.Name = "listBox1";
             this.listBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.listBox1.Size = new System.Drawing.Size(430, 147);
+            this.listBox1.Size = new System.Drawing.Size(541, 289);
             this.listBox1.TabIndex = 4;
             // 
             // installedApplicationsLabel
             // 
             this.installedApplicationsLabel.AutoSize = true;
-            this.installedApplicationsLabel.Location = new System.Drawing.Point(13, 212);
+            this.installedApplicationsLabel.Location = new System.Drawing.Point(9, 28);
             this.installedApplicationsLabel.Name = "installedApplicationsLabel";
             this.installedApplicationsLabel.Size = new System.Drawing.Size(109, 13);
             this.installedApplicationsLabel.TabIndex = 5;
@@ -95,36 +60,56 @@
             // 
             // searchBox
             // 
-            this.searchBox.Location = new System.Drawing.Point(318, 212);
+            this.searchBox.Location = new System.Drawing.Point(183, 47);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(100, 20);
+            this.searchBox.Size = new System.Drawing.Size(169, 20);
             this.searchBox.TabIndex = 6;
             this.searchBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // searchLabel
             // 
             this.searchLabel.AutoSize = true;
-            this.searchLabel.Location = new System.Drawing.Point(262, 212);
+            this.searchLabel.Location = new System.Drawing.Point(237, 28);
             this.searchLabel.Name = "searchLabel";
             this.searchLabel.Size = new System.Drawing.Size(44, 13);
             this.searchLabel.TabIndex = 7;
             this.searchLabel.Text = "Search:";
+            // 
+            // countLabel
+            // 
+            this.countLabel.AutoSize = true;
+            this.countLabel.Location = new System.Drawing.Point(12, 50);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(45, 13);
+            this.countLabel.TabIndex = 8;
+            this.countLabel.Text = "COUNT";
+            // 
+            // uninstallButton
+            // 
+            this.uninstallButton.Location = new System.Drawing.Point(461, 28);
+            this.uninstallButton.Name = "uninstallButton";
+            this.uninstallButton.Size = new System.Drawing.Size(92, 46);
+            this.uninstallButton.TabIndex = 9;
+            this.uninstallButton.Text = "Uninstall";
+            this.uninstallButton.UseVisualStyleBackColor = true;
+            this.uninstallButton.Click += new System.EventHandler(this.uninstallButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 390);
+            this.Controls.Add(this.uninstallButton);
+            this.Controls.Add(this.countLabel);
             this.Controls.Add(this.searchLabel);
             this.Controls.Add(this.searchBox);
             this.Controls.Add(this.installedApplicationsLabel);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.hostNameLabel);
-            this.Controls.Add(this.hostName);
-            this.Controls.Add(this.ipAddressLabel);
-            this.Controls.Add(this.ipLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Windows Application Manager";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -132,15 +117,12 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label ipLabel;
-        private System.Windows.Forms.Label ipAddressLabel;
-        private System.Windows.Forms.Label hostName;
-        private System.Windows.Forms.Label hostNameLabel;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label installedApplicationsLabel;
         private System.Windows.Forms.TextBox searchBox;
         private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Button uninstallButton;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using Microsoft.Win32;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace WindowsFormsApplication1
 {
@@ -19,16 +15,13 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
-            
+            countLabel.Text = "";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ipLabel.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(o => o.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).First().ToString();
-            hostName.Text = Dns.GetHostName().ToString();
 
-
-            MessageBox.Show(System.Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
+            //MessageBox.Show(System.Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"));
 
 
             //Get list of installed software
@@ -43,8 +36,10 @@ namespace WindowsFormsApplication1
                         {
                             listBox1.Items.Add(subkey.GetValue("DisplayName"));
                         }
-                        catch (Exception ex)
-                        { }
+                        catch (Exception)
+                        {
+
+                        }
                     }
 
                     programArray = new string[listBox1.Items.Count];
@@ -59,6 +54,7 @@ namespace WindowsFormsApplication1
 
                 }
             }
+            countLabel.Text = listBox1.Items.Count.ToString();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -72,6 +68,68 @@ namespace WindowsFormsApplication1
                 if (System.Text.RegularExpressions.Regex.Match(programArray[i], searchBox.Text.ToString(), RegexOptions.IgnoreCase).Success)
                 {
                     listBox1.Items.Add(programArray[i].ToString());
+                }
+            }
+            countLabel.Text = listBox1.Items.Count.ToString();
+        }
+
+
+        private void uninstallButton_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("You need to select at least one item.", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            //Get list of installed software
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        try
+                        {
+                            if(subkey.GetValue("DisplayName").ToString() == listBox1.SelectedItem.ToString())
+                            {
+                                string uninstallCommand = null;
+                                try
+                                {
+                                    uninstallCommand = subkey.GetValue("UninstallString").ToString();
+                                }
+                                catch(Exception)
+                                {
+                                    MessageBox.Show("Cann't find uninstaller for that program.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+
+                                if(uninstallCommand != null)
+                                {
+                                    //Now try to uninstall the program
+                                    Process.Start(uninstallCommand);
+                                }
+                                
+                                
+
+                            }
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+
+                    programArray = new string[listBox1.Items.Count];
+                    for (int i = 0; i < listBox1.Items.Count; i++)
+                    {
+                        object s = listBox1.Items[i];
+                        programArray[i] = s.ToString();
+                    }
+
+
+
+
                 }
             }
         }
